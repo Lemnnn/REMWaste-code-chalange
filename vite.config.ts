50ceacb24e047cdc91acd3a0db416c9fb91b8845
Andrei Lemnaru <andreilemnaru73@gmail.com>
2025-04-02 19:41:07 +0300

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://lemnnn.github.io/REMWaste-code-challenge/",
+  base: "./",
 });
